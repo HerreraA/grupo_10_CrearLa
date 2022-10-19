4e1a5c2635a8c65a9c457a6b31468f5bc64dbf2e
gabriel-laluf <glaluf77@gmail.com>
2022-10-19 01:46:11 -0300

    Modificaciones

--- app.js
+++ app.js
@@ -4,25 +4,38 @@ const app = express();
 const path = require('path');
 
 
-const publicPath= path.resolve(__dirname, "./public")
+const publicPath= path.resolve(__dirname, "./public");
+
 app.use(express.static(publicPath))
 
 app.listen (3500, () => console.log('Alta de servidor: http://localhost:3500'));
+
 app.get ('/', (req, res) => {
     let htmlPath = path.resolve(__dirname, './views/home.html');
     res.sendFile (htmlPath)
 })
 
-app.get ('/register.html', (req,res)=> {
-    res.sendFile(path.join(__dirname, './views/register.html')) })
- 
+app.get ('/register.html', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/register.html');
+    res.sendFile (htmlPath)
+})
+
 app.get('/carrito.html',(req, res) => {
     let htmlPath = path.resolve(__dirname, './views/carrito.html')
     res.sendFile(htmlPath) })
 
     
-app.get ('/product.html', (req,res)=> {
-        res.sendFile(path.join(__dirname, './views/product.html')) })
+app.get ('/product.html', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/product.html');
+    res.sendFile (htmlPath)
+})
 
-app.get ('/contact.html', (req,res)=> {
-            res.sendFile(path.join(__dirname, './views/contact.html')) })
+app.get ('/contact.html', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/contact.html');
+     res.sendFile (htmlPath)
+})
+
+app.get ('/login.html', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/login.html');
+     res.sendFile (htmlPath)
+})
