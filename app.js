b10f96d832599de9621f948fb750f0e588c0d345
Agustin-Pe <112834687+Agustin-Pe@users.noreply.github.com>
2022-10-23 23:14:04 -0300

    Implementé el motor templates

--- app.js
+++ app.js
@@ -11,31 +11,31 @@ app.use(express.static(publicPath))
 app.listen (3500, () => console.log('Alta de servidor: http://localhost:3500'));
 
 app.get ('/', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/home.html');
-    res.sendFile (htmlPath)
+    let htmlPath = path.resolve(__dirname, './views/home.ejs');
+    res.render (htmlPath)
 })
 
 app.get ('/register', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/users/register.html');
-    res.sendFile (htmlPath)
+    let htmlPath = path.resolve(__dirname, './views/users/register.ejs');
+    res.render (htmlPath)
 })
 
 app.get('/carrito',(req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/carrito.html')
-    res.sendFile(htmlPath) })
+    let htmlPath = path.resolve(__dirname, './views/carrito.ejs')
+    res.render(htmlPath) })
 
     
 app.get ('/product', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/product.html');
-    res.sendFile (htmlPath)
+    let htmlPath = path.resolve(__dirname, './views/product.ejs');
+    res.render (htmlPath)
 })
 
 app.get ('/contact', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/contact.html');
-     res.sendFile (htmlPath)
+    let htmlPath = path.resolve(__dirname, './views/contact.ejs');
+     res.render (htmlPath)
 })
 
 app.get ('/login', (req, res) => {
-    let htmlPath = path.resolve(__dirname, './views/users/login.html');
-     res.sendFile (htmlPath)
+    let htmlPath = path.resolve(__dirname, './views/users/login.ejs');
+     res.render (htmlPath)
 })
