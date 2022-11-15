766736f42991959e7bd1b79e03574f9e377d24d5
Agustin-Pe <112834687+Agustin-Pe@users.noreply.github.com>
2022-11-15 09:48:59 -0300

    ordenando app.js

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+// ************ Require's ************
 const express = require ('express');
 const { get } = require('http');
-const app = express();
 const path = require('path');
 const productsRouter= require('./routes/products');
 
+// ************ express() - (don't touch) ************
+const app = express();
 
-const publicPath= path.resolve(__dirname, "./public");
-
-app.set("view engine", "ejs");
-
+// ************ Middlewares - (don't touch) ************
 app.use(express.static(publicPath))
 
-app.listen (3500, () => console.log('Alta de servidor: http://localhost:3500'));
+
+// ************ Template Engine - (don't touch) ************
+app.set("view engine", "ejs");
+const publicPath= path.resolve(__dirname, "./public");
+
+// ************ Sistema de Rutas ************
+//const main = require('./routes/main');
+//const products = require('./routes/products');
+
+//app.use('/', main);
+//app.use('/products', products)
 
 app.get ('/', (req, res) => {
     let htmlPath = path.resolve(__dirname, './views/home.ejs');
@@ -27,7 +36,6 @@ app.get('/carrito',(req, res) => {
     let htmlPath = path.resolve(__dirname, './views/carrito.ejs')
     res.render(htmlPath) })
 
-    
 app.get ('/product', productsRouter)
 
 app.get ('/contact', (req, res) => {
@@ -39,13 +47,14 @@ app.get ('/login', (req, res) => {
     let htmlPath = path.resolve(__dirname, './views/users/login.ejs');
      res.render (htmlPath)
 })
-
 app.get ('/portafolio', (req, res) => {
     let htmlPath = path.resolve(__dirname, './views/portafolio.ejs');
      res.render (htmlPath)
 })
-
 app.get ('/diseno-web', (req, res) => {
     let htmlPath = path.resolve(__dirname, './views/products/diseno-web/diseno-web.ejs');
      res.render (htmlPath)
 })
+
+// ************ Creando servidor ************
+app.listen (3500, () => console.log('Alta de servidor: http://localhost:3500'));
