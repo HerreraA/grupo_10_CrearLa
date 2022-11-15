a757241a3b454d345b522d425fa8c144adf7f964
TaniaMelisa <taniamelisalopez@gmail.com>
2022-11-15 17:37:39 -0300

    agrego crud base en rutas y controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,11 +1,55 @@
+const fs = require('fs');
 const path = require('path');
 
-const productsController = {
+const productsFilePath = path.join(__dirname, "../data/product.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+/*const productsController = {
     index: function (req, res) {
         let htmlPath = path.resolve (__dirname, '../views/products')
         res.sendFile (htmlPath)
         
     }
-}
+}*/
+
+
+const productsController = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products', {products})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		// Do the magic
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		// Do the magic
+	},
+
+	// Create -  Method to store
+	store: (req, res) => {
+		// Do the magic
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		// Do the magic
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+	},
+
+	// Delete - Delete one product from DB
+	destroy: (req, res) => {
+		// Do the magic
+	}
+};
+
 
 module.exports = productsController ;
--- routes/products.js
+++ routes/products.js
@@ -5,4 +5,25 @@ const productsController = require('../controllers/productsController');
 router.get('/products', productsController.index);
 
 
+//*** GET ALL PRODUCTS *** 
+router.get('/', productsController.index); 
+
+//*** CREATE ONE PRODUCT *** 
+router.get('/create', productsController.create); 
+router.post('/', productsController.store); 
+
+
+//*** GET ONE PRODUCT *** 
+router.get(':id', productsController.detail); 
+
+//*** EDIT ONE PRODUCT *** 
+router.get(':id/edit', productsController.edit); 
+router.put(':id', productsController.update); 
+
+
+//*** DELETE ONE PRODUCT*** 
+router.delete(':id', productsController.destroy); 
+
+
+
 module.exports = router;
