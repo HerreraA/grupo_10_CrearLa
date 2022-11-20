1e3e55366d7fff1ba79274bda0adcd70d735582a
HerreraA <herrera.andresg@gmail.com>
2022-11-19 21:28:08 -0500

    se elimina ruta de multer que rompia el codigo

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,6 @@
 const express = require('express');
-const productsController = require('../controllers/productsController');
-const router = express.Router();
-
-const path = require('path');
-const multer = require('multer');
-
-//* Configuracion de MULTER */
-const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, '/views/images/avatars' ) // error con la carga de archivos
-    },
-    filename: (req, file, cb) =>{
-        let fileName = file.fieldname + ' ' + Date.now();  
-        cb(null, fileName )
-    }
-})
-
-const uploadFile = multer({storage})
-
 const userController = require('../controllers/usersController');
+const router = express.Router();
 
 
 //* Formulario de Login */
@@ -28,6 +10,6 @@ router.get('/login', userController.login)
 router.get('/register', userController.register)
 
 //* Procesa el registro */
-router.post('/register', uploadFile.single('foto'), userController.processRegister)
+router.post('/register', userController.processRegister)
 
 module.exports = router
