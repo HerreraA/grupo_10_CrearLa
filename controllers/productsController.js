3fe59e34a60af18627fc6bc328920cbc5af277ee
TaniaMelisa <taniamelisalopez@gmail.com>
2022-11-29 23:47:35 -0300

    cambio en crud

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -32,6 +32,11 @@ const productsController = {
     ecommerce: (req, res) => {
         res.render('./products/ecommerce.ejs')
     },
+    detail: (req, res) => {
+        const id = req.params.id;
+        const productos = products.find(product => product.id == id);
+         res.render('./products/product-noborrar.ejs', {products})
+     },
     create: (req, res)=>{
         res.render("./products/portafolio.ejs")
     },
@@ -49,8 +54,44 @@ const productsController = {
 		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, "  "));
 		res.redirect("/products")
     },
-    
+    edit: (req, res) => {
+        const id = req.params.id;
+        const productos = products.find(product => product.id == id);
+         res.render('./products/productEditForm.ejs', {products})
+     },
+     update: (req, res)=> {
+        //editamos el producto/heroe que lelgo por params.id
+        const id = req.params.id;
+        const productToEdit = products.find(product => product.id == id);
 
+        const editProduct = {
+            id: id,
+            nombre: req.body.nombre,
+            descripcion: req.body.description,
+            categoria: req.body.categoria,
+			imagen: req.file ? req.file.filename : productToEdit.imagen,
+			precio: req.body.precio
+        }
+        //ya modificamos el array
+        products.forEach((product, index) => {
+			if(product.id == id) {
+				products[index] = editProduct;
+			}
+		});
+        //** lo gaurdamos */
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+		
+		res.redirect("/products")
+    },
+    destroy: (req, res)=>{
+        //eliminamos el prodcuto que llego por params.id
+        const id = req.params.id;
+        const finalProducts = products.filter(product => product.id != id); // que me traiga todos menos al que pusimos en id
+          //** lo gaurdamos */
+        fs.writeFileSync(productsFilePath, JSON.stringify(finalproducts, null, " "));
+		
+		res.redirect("/products")
+    }
 }
 
 module.exports = productsController ;
--- routes/products.js
+++ routes/products.js
@@ -21,7 +21,7 @@ router.get('', productsController.index);
 
 //* estas rutas en realizad deberian ser para cuando buscamos por id pero hay que hacer una vista que lo permita//
 // osea esta router.get("/detail/:id", heroesCotroller.detail)
-
+router.get("/detail/:id", productsController.detail)
 router.get('/desarrolloApp', productsController.desarrolloApp);
 
 router.get('/desarrolloSoftware', productsController.desarrolloSoftware);
@@ -37,6 +37,12 @@ router.get('/ecommerce', productsController.ecommerce);
 router.get("/detail/create", productsController.create) //muestra el form//
 router.post("/detail", uploadFile.single("foto"), productsController.store); //guarda lo que cargan en el form//
 
+// ** editar un producto/heroe **
 
+router.get("/detail/edit/:id", productsController.edit)
+router.put("/detail/edit/:id", uploadFile.single("imgFile"), productsController.update)
+
+// *** ELIMINAR un producti **//
+router.delete("/detail/delete/:id", productsController.destroy)
 
 module.exports = router;
