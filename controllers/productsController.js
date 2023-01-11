f0d115051823863a54410bce82965462c95e61b0
TaniaMelisa <taniamelisalopez@gmail.com>
2023-01-11 16:21:32 -0300

    controller destroy delete

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,28 @@
 const fs = require('fs');
-const path = require('path');  
-
-const productsFilePath = path.join(__dirname, '../data/product.json');
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
- 
+const path = require('path');
+const db = require('../database/models')
+const { validationResult } = require('express-validator');
+const sequelize = db.sequelize;
 
 const productsController = {
-
+    delete: async function (req, res) {
+        try{
+            const Servicio = await Servicios.findByPk(req.params.id)
+            res.render('serviciosDelete', {Servicio})
+        }
+        catch (e) {
+            console.log(e)
+        }
+    },
+    destroy: async function (req, res) {
+        try {
+            const deleted = await Servicios.destroy({where: {id:req.params.id}, force: true})
+            res.redirect('/products')
+        }
+        catch(e) {
+            console.log(e)
+        }
+    }
 
 }
 
