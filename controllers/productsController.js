0908afb82d097632ccbbb828bbaa60b7af756247
TaniaMelisa <taniamelisalopez@gmail.com>
2023-01-11 16:51:45 -0300

    editar producto edit/update y sus rutas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,6 +5,40 @@ const { validationResult } = require('express-validator');
 const sequelize = db.sequelize;
 
 const productsController = {
+
+
+
+
+    edit: async function(req, res) {
+        try{
+            const Servicio = await Servicios.findByPk(req.params.id)
+            res.render('productEditForm', {Servicio})
+        }
+        catch (e) {
+            console.log(e)
+        }
+    },
+    update: async function (req,res) {
+        try {
+            const updated = await Servicios.update(
+                {
+                    nombre: req.body.nombre,
+                    descripcion: req.body.descripcion,
+                    imagen: req.body.imagen, //debo poner el if por si tenia? BUSCAR!
+                    precio: req.body.precio,
+                   
+                },
+                {
+                    where: {id:req.params.id}
+                }
+            )
+            res.redirect('/products/detail/' + updated.id)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    },
+
     delete: async function (req, res) {
         try{
             const Servicio = await Servicios.findByPk(req.params.id)
