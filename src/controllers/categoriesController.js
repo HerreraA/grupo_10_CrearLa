3c3718b429ba5f3b4ef78e82ac9be31b426e581f
TaniaMelisa <taniamelisalopez@gmail.com>
2023-01-26 00:34:20 -0300

    no molesta el categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -6,7 +6,7 @@ const sequelize = db.sequelize;
 
 // Defino variable para base Json de Categorías
 const categoryFilePath = path.join(__dirname, '../data/categories.json');
-let categories = JSON.parse(fs.readFileSync(categoryFilePath, 'utf-8'));
+let categorias = JSON.parse(fs.readFileSync(categoryFilePath, 'utf-8'));
 
 const serviciosFilePath = path.join(__dirname, '../data/servicios.json');
 let servicios = JSON.parse(fs.readFileSync(serviciosFilePath, 'utf-8'));
@@ -15,28 +15,28 @@ let servicios = JSON.parse(fs.readFileSync(serviciosFilePath, 'utf-8'));
 const categoriesController = {
     //* Enseña la cantidad de categorías disponibles */
     index: (req, res) => {
-        categories = JSON.parse(fs.readFileSync(categoryFilePath, 'utf-8'));
-        res.render('./servicios/categories', {categories: categories})
+        categorias = JSON.parse(fs.readFileSync(categoryFilePath, 'utf-8'));
+        res.render('./servicios/categories', {categorias: categorias})
     },
     detail: (req, res) => {
         let categoryId = req.params.id;
-        res.render('./servicios/categorySolo', {categoryId, categories});
+        res.render('./servicios/categorySolo', {categoryId, categorias});
     },
     //* Formulario para crear una categoría */
     categoryCreate: (req, res) => {
-        res.render('./servicios/categoryCreate', {categories})
+        res.render('./servicios/categoryCreate', {categorias})
     },
     //* Guarda la categoría */    
     store: (req, res)=>{
         let newCategory = {
-            id: categories[categories.length - 1].id + 1,
+            id: categorias[categorias.length - 1].id + 1,
 			nombre: req.body.nombre,
 			descripcion: req.body.description,
 			imagen: req.file.filename,
 		}
 		categories.push(newCategory);
 		fs.writeFileSync(categoryFilePath, JSON.stringify(categories, null, "  "));
-		res.redirect("/categories/all")
+		res.redirect("/categorias/all")
     }
 }
 
